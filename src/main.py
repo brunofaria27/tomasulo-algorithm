8b53b7661cd6ebdeef807b86a4b4b9037cd49416
Bruno Rodrigues Faria <80405594+brunofaria27@users.noreply.github.com>
2023-05-10 11:19:13 -0300

    remoção de códigos repetitivos

--- src/main.py
+++ src/main.py
@@ -1,23 +1,22 @@
 # Imports
 from models.Instructions import InstructionUnit
 
-# Consts
-from important_vars import INSTRUCTIONS, INSTRUCTION_QUEUE
+# Important vars
+from important_vars import INSTRUCTION_QUEUE
+
+# Global vars
+global CLOCK
 
 def readInstructions(filename: str) -> None:
     with open(filename, 'r') as file:
         for line in file:
             line_args = line.strip().split(" ")
-            INSTRUCTIONS.append([line_args[0], line_args[1], line_args[2], line_args[3]]) # OP, REGISTER, ARG1, ARG2
-
-def createInstructions() -> None:
-    for instruction in INSTRUCTIONS:
-        INSTRUCTION_QUEUE.append(InstructionUnit(instruction[0], instruction[1], instruction[2], instruction[3]))
+            INSTRUCTION_QUEUE.append(InstructionUnit(line_args[0], line_args[1], line_args[2], line_args[3])) # OP, REGISTER, ARG1, ARG2
 
 def main() -> None:
-    readInstructions("../instructions\instruction1.txt")
-    createInstructions()
+    readInstructions("../instructions/instruction1.txt")
     for instruction in INSTRUCTION_QUEUE:
         print(instruction)
+        
 
 main()
